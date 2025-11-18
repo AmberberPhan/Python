f88f848e541791a1ab45e581f55a5db53f0d6859
AmberberPhan <2002hongminh@gmail.com>
2025-11-18 20:19:15 +1100

    adding a file

---
+++ Test.py
@@ -0,0 +1,14 @@
+book = "Harry Potter"
+print(book)
+name = "Amber" 
+
+print(f"Hello {name}!")
+
+my_favourite_number = 3
+ 
+times_five = my_favourite_number * 5
+ 
+print("I multiplied your favourite number by 5 and got...")
+print(times_five)
+
+var = input
